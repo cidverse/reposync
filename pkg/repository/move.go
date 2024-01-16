9cc537ad749f21ce27726416b78a05ae11a61637
Philipp Heuer <git@philippheuer.me>
2024-01-16 23:16:00 +0100

    fix: create directory with chmod 755

--- pkg/repository/move.go
+++ pkg/repository/move.go
@@ -19,13 +19,42 @@ func MoveRepository(source string, target string) error {
 	}
 
 	// create parent dir
-	if err := os.MkdirAll(filepath.Dir(target), 0644); err != nil {
+	if err := os.MkdirAll(filepath.Dir(target), 0755); err != nil {
 		return fmt.Errorf("failed to create parent directory: %w", err)
+	}
+
+	// check permissions
+	if err := updatePermissions(filepath.Dir(source)); err != nil {
+		return fmt.Errorf("failed to update source permissions: %w", err)
+	}
+	if err := updatePermissions(filepath.Dir(target)); err != nil {
+		return fmt.Errorf("failed to update target permissions: %w", err)
 	}
 
 	// move directory
 	if err := os.Rename(source, target); err != nil {
 		return fmt.Errorf("failed to move directory: %w", err)
+	}
+
+	return nil
+}
+
+func updatePermissions(path string) error {
+	// get path information
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return fmt.Errorf("failed to get path information: %w", err)
+	}
+	if !fileInfo.IsDir() {
+		return fmt.Errorf("path is not a directory")
+	}
+
+	// update permissions if necessary
+	if fileInfo.Mode().Perm() != 0755 {
+		chmodErr := os.Chmod(path, 0755)
+		if chmodErr != nil {
+			return fmt.Errorf("failed to update permissions: %w", chmodErr)
+		}
 	}
 
 	return nil
